bb94f141a3f40b39f074b0b359f6e358b74202f5
Thomas <thomasmetias29@gmail.com>
2025-08-03 21:48:30 +0300

    feat: add header section in react environment

--- src/main.jsx
+++ src/main.jsx
@@ -1,2 +1,31 @@
 import { createRoot } from 'react-dom/client'
 
+const root = createRoot(document.getElementById("root"));
+
+function Header() {
+  return (
+      <header className="header">
+          <img src="react-logo.png" className="nav-logo" alt="React logo" />
+          <nav>
+              <ul className="nav-list">
+                  <li className="nav-list-item">Pricing</li>
+                  <li className="nav-list-item">About</li>
+                  <li className="nav-list-item">Contact</li>
+              </ul>
+          </nav>
+      </header>
+  )
+}
+
+
+function Page() {
+  return (
+      <>
+          <Header />
+      </>
+  )
+}
+
+root.render(
+  <Page />
+)
