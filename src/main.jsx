ef027715c3ae1410642aef1a8988f1d1463e9cb9
Thomas <thomasmetias29@gmail.com>
2025-08-03 21:52:02 +0300

    feat: add MainContent and Footer to react code

--- src/main.jsx
+++ src/main.jsx
@@ -17,11 +17,32 @@ function Header() {
   )
 }
 
+function MainContent() {
+  return (
+      <main>
+          <h1>Reason I am excited to learn React</h1>
+          <ol>
+              <li>React is a popular library, so I will be able to fit in with all the coolest devs out there! 😎</li>
+              <li>I am more likely to get a job as a front end developer if I know React</li>
+          </ol>
+      </main>
+  )
+}
+
+function Footer() {
+  return (
+      <footer>
+          © 2025 thomas development. All rights reserved.
+      </footer>
+  )
+}
 
 function Page() {
   return (
       <>
           <Header />
+          <MainContent />
+          <Footer />
       </>
   )
 }
